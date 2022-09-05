fe96b3ba6562e2c2b39b9e836673db9d5201584a
去年夏天 <tjsky@foxmail.com>
2022-09-05 10:08:58 +0800

    修改默认代理

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,10 +1,5 @@
 export const proxy = [
-  'https://api.13pikpak.cf',
-  'https://api.14pikpak.cf',
-  'https://api.15pikpak.cf',
-  'https://api.16pikpak.cf',
-  'https://api.17pikpak.cf',
-  'https://api.18pikpak.cf',
+  'https://api-pikpak.go-back.cf',
 ]
 
 export const version = '1.0.0'
