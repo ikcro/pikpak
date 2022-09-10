f491826df993a516a7eaa1f2b4500aafa129d9fe
去年夏天 <tjsky@foxmail.com>
2022-09-10 13:29:04 +0800

    增加反代地址

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,6 +3,7 @@ export const proxy = [
   'https://diii.tk',
   'https://api-pikpak.go-back.cf',
   'https://api-pikpak.tjsky.cf',
+  'https://api-pikpak.tjsky-1.cf',
 ]
 
 export const version = '1.23.0'
