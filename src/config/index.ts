8889d945f1605a0ba3c26cd8a80a86f4037c5b1a
去年夏天 <tjsky@foxmail.com>
2022-09-15 17:24:12 +0800

    去掉失效的反代地址

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,5 @@
 export const proxy = [
   'https://diii.tk',
-  'https://api-pikpak.go-back.cf',
   'https://api-pikpak.tjsky.cf',
   'https://api-pikpak.tjsky-1.cf',
   'https://pikpak.he13797739994.ga',
