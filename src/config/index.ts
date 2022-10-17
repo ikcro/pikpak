244279beef3b187314284322903a87881aeaca13
去年夏天 <tjsky@foxmail.com>
2022-10-17 22:11:24 +0800

    去掉失效反代

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,7 +2,6 @@ export const proxy = [
   'https://diii.tk',
   'https://api-pikpak.tjsky.cf',
   'https://api-pikpak.tjsky-1.cf',
-  'https://pikpak.he13797739994.ga',
   'https://pikpak.hdddd.me',
   'https://pan.yooyi.tk',
   'https://pikpak.nsso.eu.org',
